f4934d44dd60886133e472d4275be26785b385ff
Ergo Kannikka <ergokannikka@gmail.com>
2024-06-11 19:49:53 +0000

    code

--- rest-parameter-syntax.js
+++ rest-parameter-syntax.js
@@ -4,7 +4,16 @@
  */
 
 // Regular function call 
-
+const sumAll = (a, b, c) => a + b + c;
 // Extra arguments are ignored
 
 // Function using ...rest
+const sumAll2 = (a, b, c, ...rest) => {
+    let sum = a + b + c; 
+    for (let i of rest) {
+        sum += i;
+    }
+    return sum;
+}
+let sumAll3 = sumAll2(1,2,3,4,5,6);
+console.log(sumAll3)
